b314ac35504e508b5c19de6cfa64665cc5b7246c
orion-miller <33074666+orion-miller@users.noreply.github.com>
2023-12-21 22:54:33 -0500

    array and commetning examples

---
+++ Module2/Commenting/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string str = "The quick brown fox jumps over the lazy dog.";
+
+// convert the message into a char array
+char[] originalMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(originalMessage);
+
+int oCount = 0;
+
+// count the o's
+foreach (char i in originalMessage) 
+{ 
+    if (i == 'o') 
+    { 
+        oCount++; 
+    } 
+}
+
+// convert it back to a string
+string newMessage = new String(originalMessage);
+
+// print it out
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {oCount} times.");
