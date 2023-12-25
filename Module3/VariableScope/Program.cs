27b82c3110738b628aa94b856534496e5597af28
orion-miller <33074666+orion-miller@users.noreply.github.com>
2023-12-24 23:18:57 -0500

    variable scope example

--- Module3/VariableScope/Program.cs
+++ Module3/VariableScope/Program.cs
@@ -1,7 +1,33 @@
-﻿bool flag = true;
-if (flag)
+﻿// bool flag = true;
+// if (flag)
+// {
+//     int value = 10;
+//     Console.WriteLine($"Inside of code block: {value}");
+// }
+// Console.WriteLine($"Outside of code block: {value}");
+
+//==============================================================
+
+// bool flag = true;
+// if (flag)
+//     Console.WriteLine(flag);
+//     Console.WriteLine(flag + " test");
+
+//==============================================================
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (int number in numbers)
 {
-    int value = 10;
-    Console.WriteLine($"Inside of code block: {value}");
+    total += number;
+
+    if (number == 42)
+        found = true;
 }
-Console.WriteLine($"Outside of code block: {value}");
+
+if (found) 
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
