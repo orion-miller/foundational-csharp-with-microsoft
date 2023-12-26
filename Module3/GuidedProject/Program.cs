688c0e820468d16a13022fa418f3a1760ab2dfe1
orion-miller <33074666+orion-miller@users.noreply.github.com>
2023-12-25 20:41:10 -0500

    challenge project

---
+++ Module3/GuidedProject/Program.cs
@@ -0,0 +1,15 @@
+﻿// the ourAnimals array will store the following: 
+string animalSpecies = "";
+string animalID = "";
+string animalAge = "";
+string animalPhysicalDescription = "";
+string animalPersonalityDescription = "";
+string animalNickname = "";
+
+// variables that support data entry
+int maxPets = 8;
+string? readResult;
+string menuSelection = "";
+
+// array used to store runtime data, there is no persisted data
+string[,] ourAnimals = new string[maxPets, 6];
